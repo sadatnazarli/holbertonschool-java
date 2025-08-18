945fa0d9fdf012dc0535d12879d92b07875354ad
Sadat <sedat.nazarli@gmail.com>
2025-08-18 17:17:24 +0000

    task 1 complated

---
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -0,0 +1,9 @@
+public class PrintIntegerVariable {
+	public static void main(String[] args) {
+		String name = "Pennsylvania Avenue";
+		
+		int number = 1600;
+
+		System.out.println(name + ", " +  number);
+	}
+}
