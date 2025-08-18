b483c13d61e1644df1e1d8c36dff0a12f534fb16
Sadat <sedat.nazarli@gmail.com>
2025-08-18 18:13:37 +0000

    task 4 finished

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,8 @@
+public class PrintStringConcat {
+	public static void main(String[]  args) {
+		String text1 = "Holberton";
+		String text2 = "School";
+		String concatenatedVariable = text1 + " " + text2;
+		System.out.println("Welcome to " + concatenatedVariable + " !");
+	}
+}
